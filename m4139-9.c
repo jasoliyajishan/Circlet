958bfe395334f51c5c6fd3067c1f4b23a58b90fb
jasoliyajishan <132878572+jasoliyajishan@users.noreply.github.com>
2023-05-25 18:18:47 +0530

    Add files via upload

---
+++ m4139-9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+ main() {
+    int n=49, i, j;
+
+  
+   
+
+    for (i = 45; i <= n; i++) 
+	{
+        for (j = 45; j <= i; j++) 
+		{
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
