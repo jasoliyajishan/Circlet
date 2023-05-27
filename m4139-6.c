8b5d50faff8cc181d6597f0a0d624aa1a3c07ba1
jasoliyajishan <132878572+jasoliyajishan@users.noreply.github.com>
2023-05-26 23:55:59 -0700

    Add files via upload

--- m4139-6.c
+++ m4139-6.c
@@ -1,16 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
-main()
-{
-	int i,j;
-	for(i=5;i>=1;i--)
+int main() {
+    int i, j;
+
+    for (i = 5; i >= 1; i--) 
 	{
-	  for(j=i;j>=1;j--)
-	  {
-	  	printf("%d",j);
-	  }
-	  printf("\n");
-	}
-	  
-	 
+        for (j = 1; j < i; j++) 
+		{
+            printf(" ");
+        }
+
+        for (j = i; j <= 5; j++)
+		 {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
 }
+
