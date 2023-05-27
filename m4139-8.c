1ecc8e743e11c1eedfcc669b077967e44323c70e
jasoliyajishan <132878572+jasoliyajishan@users.noreply.github.com>
2023-05-27 00:14:02 -0700

    Add files via upload

--- m4139-8.c
+++ m4139-8.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 
-int main() 
-{
-    int n=5, i, j;
+main() {
+    int i, j;
 
-    
-
-    for (i = 1; i <= n; i++)
+    for (i = 1; i <= 5; i++)
 	 {
-        for (j = i; j <= n; j++)
+        for (j = 1; j <= 5 - i; j++)
+		 {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++)
 		 {
             printf("%d", j);
         }
+
+        printf("\n");
+    }
+
+    for (i = 2; i <= 5; i++) 
+	{
+        for (j = 1; j < i; j++)
+		 {
+            printf(" ");
+        }
+
+        for (j = i; j <= 5; j++) 
+		{
+            printf("%d", j);
+        }
+
         printf("\n");
     }
 
     return 0;
 }
+
